00d96c02ef9017b41c7c6c6b0e04d086da8eab1d
neominal.solutions <neominal.solutions@gmail.com>
2024-04-03 21:40:04 +0300

    env dosyaları güncellendi.

--- src/pages/envDemo/EnvSample.tsx
+++ src/pages/envDemo/EnvSample.tsx
@@ -1,6 +1,13 @@
+import { env } from 'process';
 import React from 'react';
+
 function EnvSample() {
-	return <div>Bucket: {process.env.REACT_APP_ENV_S3_BUCKET}</div>;
+	return (
+		<div>
+			<div>Env: {process.env.REACT_APP_ENV}</div>
+			<div>Bucket: {process.env.REACT_APP_ENV_S3_BUCKET}</div>
+		</div>
+	);
 }
 
 export default EnvSample;
