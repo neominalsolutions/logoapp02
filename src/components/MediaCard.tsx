0e4d87bbf8943f9dcfebb8aa74e61aefd371ebad
neominal.solutions <neominal.solutions@gmail.com>
2024-04-03 19:12:12 +0300

    ESLint ve Pretier eklendi.

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,65 +1,65 @@
 import {
-	Button,
-	Card,
-	CardActions,
-	CardContent,
-	CardMedia,
-	Typography,
+ Button,
+ Card,
+ CardActions,
+ CardContent,
+ CardMedia,
+ Typography,
 } from '@mui/material';
 
 export type MediaButton = {
-	text: string;
-	icon?: string;
-	type: string;
+ text: string;
+ icon?: string;
+ type: string;
 };
 
 export type MediaCardProps = {
-	imageUrl: string;
-	title: string;
-	description: string;
-	buttons: MediaButton[];
-	onButtonClick?: (type: string) => void; // action Props
+ imageUrl: string;
+ title: string;
+ description: string;
+ buttons: MediaButton[];
+ onButtonClick?: (type: string) => void; // action Props
 };
 
 export default function MediaCard({
-	imageUrl,
-	title,
-	description,
-	buttons,
-	onButtonClick,
+ imageUrl,
+ title,
+ description,
+ buttons,
+ onButtonClick,
 }: MediaCardProps) {
-	return (
-		<Card sx={{ maxWidth: 345 }}>
-			<CardMedia sx={{ height: 140 }} image={imageUrl} title="green iguana" />
-			<CardContent>
-				<Typography gutterBottom variant="h5" component="div">
-					{title}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					{description}
-				</Typography>
-			</CardContent>
-			<CardActions>
-				{buttons.map((btn: MediaButton, index: number) => {
-					return (
-						<span key={index}>
-							<Button
-								onClick={() => {
-									// hangi butonun hangi tipte fırlatılacağını gönderdik.
-									// onButtonClick prop undefined değilse çalışacak
-									if (onButtonClick) {
-										onButtonClick(btn.type);
-									}
-								}}
-								key={index}
-								size="small"
-							>
-								{btn.text}
-							</Button>
-						</span>
-					);
-				})}
-			</CardActions>
-		</Card>
-	);
+ return (
+  <Card sx={{ maxWidth: 345 }}>
+   <CardMedia sx={{ height: 140 }} image={imageUrl} title="green iguana" />
+   <CardContent>
+    <Typography gutterBottom variant="h5" component="div">
+     {title}
+    </Typography>
+    <Typography variant="body2" color="text.secondary">
+     {description}
+    </Typography>
+   </CardContent>
+   <CardActions>
+    {buttons.map((btn: MediaButton, index: number) => {
+     return (
+      <span key={index}>
+       <Button
+        onClick={() => {
+         // hangi butonun hangi tipte fırlatılacağını gönderdik.
+         // onButtonClick prop undefined değilse çalışacak
+         if (onButtonClick) {
+          onButtonClick(btn.type);
+         }
+        }}
+        key={index}
+        size="small"
+       >
+        {btn.text}
+       </Button>
+      </span>
+     );
+    })}
+   </CardActions>
+  </Card>
+ );
 }
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,17 +1,17 @@
 import { Grid } from '@mui/material';
 
 type RowProps = {
-	children?: React.ReactNode;
+ children?: React.ReactNode;
 };
 
 // yani children Props ile bir component içirisinde açma kapama tagleri içerisinde çalışan bir component dizayn ettik
 
 export default function Row({ children }: RowProps) {
-	return (
-		<>
-			<Grid sx={{ padding: '5px' }} spacing={2}>
-				{children}
-			</Grid>
-		</>
-	);
+ return (
+  <>
+   <Grid sx={{ padding: '5px' }} spacing={2}>
+    {children}
+   </Grid>
+  </>
+ );
 }
